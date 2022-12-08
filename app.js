bfb28ec66b0a6298101a17c5fea57a9a981a63b2
franlagorio <franlagorio@gmail.com>
2022-12-08 18:51:01 -0300

    final changes

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ class UI {
     const productList = document.getElementById("product-list");
     const element = document.createElement("div");
     element.innerHTML = `
-    <div class="card">
+    <div class="card" id="${product.name}">
     <div class="card-body text-center">
       <strong class="card-title">Product:</strong> ${product.name}
       <strong class="card-text">Price:</strong> $${product.price}
@@ -26,12 +26,28 @@ class UI {
     this.resetForm();
 
     this.showMessage("Product Added Successfully", "success");
+    const productsStorage = JSON.parse(localStorage.getItem("products"));
+    localStorage.setItem(
+      "products",
+      JSON.stringify([...productsStorage, product])
+    );
   }
 
   deleteProduct(element) {
     if (element.name === "delete") {
-      element.parentElement.parentElement.parentElement.remove();
+      element.parentElement.parentElement.remove();
+      let products = JSON.parse(localStorage.getItem("products"));
+
+      let a = element.parentElement.parentElement;
+      console.log(a.id);
+
+      let filteredProducts = products.filter(
+        (product) => product.name !== a.id
+      );
+      localStorage.setItem("products", JSON.stringify(filteredProducts));
     }
+
+    this.showMessage("Product Deleted Successfully", "warning");
   }
 
   showMessage(message, cssClass) {
@@ -42,8 +58,11 @@ class UI {
     //Show in DOM
     const container = document.querySelector(".container");
     const app = document.querySelector("#App");
-
     container.insertBefore(div, app);
+
+    setTimeout(() => {
+      document.querySelector(".alert").remove();
+    }, 2000);
   }
 
   resetForm() {
@@ -54,13 +73,16 @@ class UI {
 document.getElementById("product-form").addEventListener("submit", (event) => {
   event.preventDefault();
 
+  const ui = new UI();
+
   const name = document.getElementById("name").value;
   const price = document.getElementById("price").value;
   const year = document.getElementById("year").value;
 
+  if (name === "" || price === "" || year === "") {
+    return ui.showMessage("Please enter all inputs", "danger");
+  }
   const product = new Product(name, price, year);
-
-  const ui = new UI();
 
   ui.addProduct(product);
 });
@@ -69,3 +91,41 @@ document.getElementById("product-list").addEventListener("click", (e) => {
   const ui = new UI();
   ui.deleteProduct(e.target);
 });
+
+class Starter {
+  constructor() {
+    this.products = [];
+  }
+
+  getProducts() {
+    let productStorage = localStorage.getItem("products");
+    if (!productStorage) {
+      localStorage.setItem("products", JSON.stringify(this.products));
+    } else {
+      this.products = JSON.parse(productStorage);
+    }
+
+    const divProducts = document.createElement("div");
+
+    this.products.map((product) => {
+      divProducts.innerHTML += `
+      <div class="card" id="${product.name}">
+        <div class="card-body text-center">
+          <strong class="card-title">Product:</strong> ${product.name}
+          <strong class="card-text">Price:</strong> $${product.price}
+          <strong class="card-text">Product Year:</strong> ${product.year}
+          <br/>
+          <a name="delete" class="btn btn-danger">Delete</a>
+        </div>
+      </div>
+      `;
+    });
+
+    const div = document.getElementById("product-list");
+
+    div.appendChild(divProducts);
+  }
+}
+
+const StartApp = new Starter();
+StartApp.getProducts();
